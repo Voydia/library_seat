bb61c4ffe788a03f902410d00933213644173605
肆静 <1536421373@qq.com>
2026-04-27 13:07:04 +0800

    初始化图书馆智能选座项目

---
+++ src/main/java/com/voydia/LibrarySeatApplication.java
@@ -0,0 +1,13 @@
+package com.voydia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voydia/LibrarySeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voydia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
