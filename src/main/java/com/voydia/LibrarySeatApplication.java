d675ad26a4b69f361e9906bbd95c46fef88b6923
肆静 <1536421373@qq.com>
2026-04-28 20:14:25 +0800

    初始化图书馆智能选座项目

--- src/main/java/com/voydia/LibrarySeatApplication.java
+++ src/main/java/com/voydia/LibrarySeatApplication.java
@@ -1,12 +1,15 @@
 package com.voydia;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.voydia.mapper")
 public class LibrarySeatApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(LibrarySeatApplication.class, args);
     }
 
